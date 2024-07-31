8f62013e04a62b5056e8587318cc71e197da99d3
Chihiro88G <1114chihiro@gmail.com>
2024-07-31 09:35:07 -0400

    changed itenerary content size in db

--- database/9-itenerary.sql
+++ database/9-itenerary.sql
@@ -4,7 +4,7 @@ CREATE TABLE itenerary (
   id integer PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(255) NOT NULL,
   included_meal_id integer NOT NULL,
-  content VARCHAR(255) NOT NULL,
+  content VARCHAR(1000) NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT NOW(),
   edited TIMESTAMP NOT NULL DEFAULT NOW(),
   FOREIGN KEY (included_meal_id) REFERENCES meals(id)
@@ -14,5 +14,5 @@ INSERT INTO itenerary (title, included_meal_id, content)
 VALUES 
 ('Arrival in Tokyo', 1, 'Enjoy a free day in Tokyo or add an excursion.'),
 ('Arrival in Kyoto', 2, 'Enjoy a free day in Kyoto or add an excursion.'),
-('Sightseeing tour of Tokyo', 3, "Embark on your tour of Japan's capital.\r\n- Make a photo stop and view the iconic Shibuya crossing from overhead as you drive through the central part of the city.")
+('Sightseeing tour of Tokyo', 3, "Embark on your tour of Japan's capital.\r\n- Make a photo stop and view the iconic Shibuya crossing from overhead as you drive through the central part of the city.\r\n- Set off on a short walk from Yoyogi Park to the Meiji Shrine, final resting place of Emperor Meiji\r\n- Stroll through the colorful Harajuku neighborhood, where Tokyo’s history and present-day pop culture intertwine")
 ;
