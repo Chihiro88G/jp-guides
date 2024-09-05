297a08c23476e12f8de10fbabc4618a4fadf9c4d
Chihiro88G <1114chihiro@gmail.com>
2024-09-05 14:06:10 -0400

    hid about page, updated favicon

--- frontend/src/utils/getPages.tsx
+++ frontend/src/utils/getPages.tsx
@@ -7,7 +7,7 @@ export function getPages(): Page[] {
   const pages: Page[] = [
     { name: 'Home', path: '/' },
     { name: 'Tours', path: '/tours' },
-    { name: 'About', path: '/about' },
+    // { name: 'About', path: '/about' },
     { name: 'Contact', path: '/contact' },
   ]
   
