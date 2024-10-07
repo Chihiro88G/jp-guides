8e95733f4d5dd5b8c0e95f0835558efb1702b028
Chihiro88G <1114chihiro@gmail.com>
2024-10-06 20:54:46 -0400

    added loading and error for TravelTips

--- frontend/src/components/home/TravelTips.tsx
+++ frontend/src/components/home/TravelTips.tsx
@@ -1,34 +1,57 @@
 import { useEffect, useState } from 'react';
-import { Box } from '@mui/material';
 import { TravelTipsType } from '../../types/travelTips';
 import Title from '../Title';
 import SectionWrapper from '../SectionWrapper';
 import AccordionItem from './AccordionItem';
 import TravelTipsWrapper from './TravelTipsWrapper';
+import LoadingSpinner from '../LoadingSpinner';
 
 export default function TravelTips() {
-  const [data, setData] = useState<TravelTipsType[]>([]);
-  
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URI}/travel-tips`)
-    .then(res => res.json())
-    .then((data: TravelTipsType[]) => setData(data));
-  }, []);
-
-  if (data.length < 1) return <Box>No Travel Tips</Box>
-  
   return (
     <SectionWrapper>
       <TravelTipsWrapper>
         <Title>Travel Tips for Japan</Title>
-        {data.map((item: TravelTipsType) => (
-          <AccordionItem
-            key={item.title}
-            topSummary={item.title}
-            details={item.content}
-          />
-        ))}
+        <RenderTravelTips />
       </TravelTipsWrapper>
     </SectionWrapper>
+  );
+}
+
+function RenderTravelTips() {
+  const [items, setItems] = useState<TravelTipsType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  
+  useEffect(() => {
+    setLoading(true);
+    fetch(`${process.env.REACT_APP_API_URI}/travel-tips`)
+    .then(res => {
+      if (!res.ok) throw new Error('failed to fetch travel tips');
+      return res.json();
+    })
+    .then((data: TravelTipsType[]) => {
+      setItems(data)
+      setLoading(false);
+    })
+    .catch(err => {
+      setError(err);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <LoadingSpinner />;
+  if (error) return null;
+  if (items.length < 1) return null;
+  
+  return (
+    <>
+      {items.map((item: TravelTipsType) => (
+        <AccordionItem
+          key={item.title}
+          topSummary={item.title}
+          details={item.content}
+        />
+      ))}
+    </>
   )
 }
